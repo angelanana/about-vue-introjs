540e4251ed7d0a8ca0af8879cd7060f0f17c8eeb
nana.zheng <nana.zheng@rccchina.com>
2021-09-07 17:39:36 +0800

    master 添加介绍页面

---
+++ src/components/introduce.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="introduce-container"> 
+    <div class="intro-header">
+      文章标题
+    </div>
+    <div class="intro-content">
+      文章内容
+    </div>
+    <div class="intro-footer">
+      文章底部
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
